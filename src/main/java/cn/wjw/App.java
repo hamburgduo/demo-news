383232018064819c441f5e305019f7dbabcefbd0
hamburgduo <kanbaoduo@163.com>
2019-07-26 16:23:13 +0800

    support war deploy on tomcat.

--- src/main/java/cn/wjw/App.java
+++ src/main/java/cn/wjw/App.java
@@ -2,11 +2,20 @@ package cn.wjw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;																 
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;																		 
 
 @SpringBootApplication
 @ServletComponentScan  //springboot启动时会扫描webservlet注解并实例化
-public class App {
+public class App extends SpringBootServletInitializer {	  
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		application.sources(this.getClass());
+		return super.configure(application);
+    }
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
